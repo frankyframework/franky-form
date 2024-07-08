95b625e2c1eb24656b62badb496354b0ded50ec1
ussiel <ussiel@gmail.com>
2024-07-08 10:32:06 -0600

    add show password

--- src/Form.php
+++ src/Form.php
@@ -33,7 +33,7 @@ class Form
     private $InputRadio;
     private $InputCheck;
     private $InputDate;
-    private $InputMobile;
+    private $InputDateMobile;
     private $InputLabel;
     private $isMobile;
 
--- src/InputPassword.php
+++ src/InputPassword.php
@@ -5,6 +5,7 @@ namespace Franky\Form;
 class InputPassword{
 
     private $name = '';
+    private $labelShow = '';
     private $attrs = [];
 
     public function __construct() {
@@ -41,7 +42,9 @@ class InputPassword{
             $this->attrs[$attr] = $val;
             return $this;
         }
-
+        if(!isset($this->attrs[$attr])){
+            return "";
+        }
         return $this->attrs[$attr];
     }
 
@@ -53,7 +56,9 @@ class InputPassword{
         {
             foreach($this->attrs as $k => $v)
             {
-                $txt .= $k.'="'.$v.'" ';
+                if($k != "showLabel") {
+                    $txt .= $k.'="'.$v.'" ';
+                }
             }
         }
 
@@ -63,24 +68,28 @@ class InputPassword{
     public function script()
     {
          return '<script >'
-                . '$(".show_password").click(function(){'
-                .   'if($(this).is(":checked")) { $(this).parent().prev("input").attr("type", "text"); } '
-                .   'else { $(this).parent().prev("input").attr("type", "password"); } '
+                . '$(".show_password'.$this->name().'").click(function(){'
+                .   'if($(this).is(":checked")) { $("input[name='.$this->name().']").attr("type", "text"); } '
+                .   'else { $("input[name='.$this->name().']").attr("type", "password"); } '
                 . '});'
                 . '</script>';
     }
 
     public function eye()
     {
-         return '<label><input type="checkbox" name="valueshowpassword" class="show_password"/>Mostrar valor</label>';
+         return '<label><input type="checkbox" name="valueshowpassword'.$this->name().'" class="show_password'.$this->name().'"/>'.(!empty($this->attr('showPassword')) ? $this->attr('showPassword') : 'Mostrar valor').'</label>';
     }
 
+    public function errorLabel()
+    {
+        return '<label id="'.$this->name().'-error" class="error" for="'.$this->name().'" style="display: none;"></label>';
+    }
     public function create()
     {
         $type = "password";
       
 
-        return '<input type="'.$type.'" name="'.$this->name().'"  '.$this->attrs2txt().' />'.$this->eye().$this->script();
+        return '<input type="'.$type.'" name="'.$this->name().'"  '.$this->attrs2txt().' />'.$this->eye().$this->errorLabel().$this->script();
     }
 
 }
